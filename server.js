34b82f78a9d52e8c306850ca5c60454c2972f274
NicolasPotesG <n.potes@uniandes.edu.co>
2021-11-13 13:11:40 -0500

    failed multer

--- server.js
+++ server.js
@@ -1,45 +1,6 @@
-//Packages for img
-// const multer = require('multer');
-// const path = require('path');
-// const model = require('./model');
-
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = require("./index");
-
-//configure multer storage
-// app.use('/uploads', express.static(__dirname + '/uploads'));
-// var storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, 'uploads');
-//   },
-
-//   filename: function (req, file, cb) {
-//     cb(null, new Date().toISOString() + file.originalname);
-//   }
-// });
-
-// var upload = multer({ storage: storage });
-// app.post('/upload', upload.single('myFile'), async (req, res, next) => {
-//   const file = req.file;
-//   if (!file) {
-//     const error = new Error('Please upload a file');
-//     error.httpStatusCode = 400;
-//     return next("hey error");
-//   }
-//   const imagepost = new model({
-//     image: file.path
-//   });
-
-//   const savedimage = await imagepost.save();
-//   res.json(savedimage);
-// })
-// app.get('/image', async (req, res) => {
-//   const image = await model.find();
-//   res.json(image);
-// });
-
-//
 
 dotenv.config({
   path: "./config.env",
@@ -65,6 +26,5 @@ const conect = async () => {
     console.log(`App running on mode ${process.env.NODE_ENV} on port ${port}...`);
   });
 }
-
 
 conect();
